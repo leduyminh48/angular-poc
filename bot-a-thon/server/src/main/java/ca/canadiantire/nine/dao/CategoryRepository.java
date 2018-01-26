32c5fd06f95132d09b5f9fd0291794ce63bf4755
Viktor Letunovskyi <Viktor_Letunovskyi@epam.com>
2018-01-26 19:04:30 +0200

    Unit test for ProductController.getProductById()

--- bot-a-thon/server/src/main/java/ca/canadiantire/nine/dao/CategoryRepository.java
+++ bot-a-thon/server/src/main/java/ca/canadiantire/nine/dao/CategoryRepository.java
@@ -1,6 +1,7 @@
 package ca.canadiantire.nine.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import ca.canadiantire.nine.domain.Category;
 
---
+++ bot-a-thon/server/src/test/java/ca/canadiantire/nine/web/ProductControllerTest.java
@@ -0,0 +1,44 @@
+package ca.canadiantire.nine.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import ca.canadiantire.nine.dao.CategoryRepository;
+import ca.canadiantire.nine.dao.ProductRepository;
+import ca.canadiantire.nine.domain.Product;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Victor Letunovsky
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    @SuppressWarnings("unused")
+    private CategoryRepository categoryRepository;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Test
+    public void shouldGetProductById() throws Exception {
+        when(productRepository.findOne(1L)).thenReturn(new Product());
+        mockMvc.perform(get("/product/1")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+}
