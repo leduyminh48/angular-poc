2fb013a360ca8d240ff28b27a6807bc767da2cb8
Viktor Letunovskyi <Viktor_Letunovskyi@epam.com>
2018-01-26 20:35:38 +0200

    Added RecurringItemRepository.getRecurringItemsByTemplateId

--- bot-a-thon/server/src/main/java/ca/canadiantire/nine/dao/RecurringItemRepository.java
+++ bot-a-thon/server/src/main/java/ca/canadiantire/nine/dao/RecurringItemRepository.java
@@ -5,9 +5,14 @@ package ca.canadiantire.nine.dao;
  * Copyright (c) 2018. Canadian Tire Corporation, Ltd. All rights reserved.
  */
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.canadiantire.nine.domain.RecurringItem;
 
 public interface RecurringItemRepository extends CrudRepository<RecurringItem, Long> {
+
+    List<RecurringItem> getRecurringItemsByTemplateId(Long recurringTemplateId);
+
 }
--- bot-a-thon/server/src/main/resources/data.sql
+++ bot-a-thon/server/src/main/resources/data.sql
@@ -25,9 +25,9 @@ INSERT INTO product (category_id, name, price, image_uri) VALUES (2, 'Toilet pap
 INSERT INTO product (category_id, name, price, image_uri) VALUES (2, 'Toothpaste', 2.38, null);
 INSERT INTO product (category_id, name, price, image_uri) VALUES (2, 'Toothbrush', 1.73, null);
 INSERT INTO product (category_id, name, price, image_uri) VALUES (3, 'Notebook', 1.54, 'notebook.jpg');
-INSERT INTO product (category_id, name, price, image_uri) VALUES (3, 'Pen', 0.80, null);
-INSERT INTO product (category_id, name, price, image_uri) VALUES (3, 'Scissors', 4.40, null);
-INSERT INTO product (category_id, name, price, image_uri) VALUES (4, 'Tassimo pods', 3.24, null);
+INSERT INTO product (category_id, name, price, image_uri) VALUES (3, 'Pen', 0.80, 'pen.jpg');
+INSERT INTO product (category_id, name, price, image_uri) VALUES (3, 'Scissors', 4.40, 'scissors.jpg');
+INSERT INTO product (category_id, name, price, image_uri) VALUES (4, 'Tassimo pods', 3.24, 'tassimo_pods.jpg');
 INSERT INTO product (category_id, name, price, image_uri) VALUES (4, 'Keurig k-cups',8.50, 'keurig_k-cups.jpg');
 INSERT INTO product (category_id, name, price, image_uri) VALUES (4, 'Tea', 0.70, null);
 INSERT INTO product (category_id, name, price, image_uri) VALUES (5, 'Laundry detergent', 7.35, null);
